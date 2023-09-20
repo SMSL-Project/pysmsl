0c0b21c792cb94923539645da41930a7bcce7907
Yihao Liu <yliu333@jhu.edu>
2023-09-20 16:43:23 -0400

    using CFG to parse?

--- src/smsl/smsl_interperter.py
+++
@@ -1,8 +0,0 @@
-class Interpreter:
-    def visit(self, node):
-        method_name = f'visit_{type(node).__name__}'
-        method = getattr(self, method_name, self.no_visit_method)
-        return method(node)
-    
-    def no_visit_method(self, node):
-        raise Exception(f'No visit_{type(node).__name__} method defined')
--- src/smsl/smsl_loader.py
+++
@@ -1,7 +0,0 @@
-class Loader():
-
-    def __init__(self):
-        pass
-
-    def load(self):
-        
