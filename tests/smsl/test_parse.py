fe0cb8e75f64066d457847d6eb881851a4c77193
Yihao Liu <yliu333@jhu.edu>
2023-09-29 14:22:33 -0400

    following tutorial https://github.com/davidcallanan/py-myopl-code/blob/master/ep14/basic.py#L185

---
+++ tests/smsl/test_parse.py
@@ -0,0 +1,24 @@
+def run(fn, text):
+    """
+    Test parsing structured text to SMSL data
+    """
+    
+    global_symbol_table = SymbolTable()
+
+    # Generate tokens
+    lexer = Lexer(fn, text)
+    tokens, error = lexer.make_tokens()
+    if error: return None, error
+
+    # Generate AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+    if ast.error: return None, ast.error
+
+    # Run program
+    interpreter = Interpreter()
+    context = Context('<program>')
+    context.symbol_table = global_symbol_table
+    result = interpreter.visit(ast.node, context)
+
+    return result.value, result.error
