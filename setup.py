d9094f117042935a9238f512fe81a0a485f6cdba
Yihao Liu <yihao@Yihaos-MacBook-Pro.local>
2025-04-19 16:08:37 -0500

    pip install

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pysmsl',
+    version='0.0.1',
+    packages=find_packages(exclude=['smsl.smsl_parser', 'smsl.smsl_parser.*']),
+    install_requires=[
+        'pyyaml',
+        'networkx',
+        'matplotlib',
+    ],
+    author='Yihao Liu',
+    author_email='yihao.jhu@gmail.com',
+    description='State Machine Serialization Language',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/SMSL-Project/pysmsl',
+    license='MIT',
+)
---
+++ smsl/__init__.py
@@ -0,0 +1,7 @@
+# smsl/__init__.py
+from .smsl import smslStateMachine
+from .smsl_state import smslState
+from .smsl_operation import smslOperation
+from .smsl_state_branch import smslStateBranch
+
+__version__ = '0.1.0'
